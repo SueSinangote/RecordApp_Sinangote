6831b932e9e36cca89fbc116bcbed5b70e8b4d32
SueSinangote <202080201@psu.palawan.edu.ph>
2022-10-23 16:28:10 +0800

    record app done

---
+++ includes/navbar.php
@@ -0,0 +1,71 @@
+<nav class="navbar navbar-expand-lg" color-on-scroll="500">
+    <div class="contain-fluid">
+        <a class="navbar-brand" href="#pablo">Template</a>
+        <button href="" class="navbar-toggler-right" type="button" data-toggle="colapse" aria-controls=">
+            <span class="navbar-toggler-bar burger-lines"></span>
+            <span class="navbar-toggler-bar burger-lines"></span>
+            <span class="navbar-toggler-bar burger-lines"></span>
+        </button>
+        <div class="collapse navbar-collapse justify-content-end" id="navigation">
+            <ul class="nav navbar-nav mr-auto">
+                <li class="nav-item">
+                    <a href="#" class="nav-link" data-toggle="dropdown">
+                        <i class="nc-icon nc-palette"></i>
+                        <span class="d-lg-none">Dashboard</span>
+                    </a>
+                </li>
+                <li class="dropdown nav-item">
+                    <a href="#" class="dropdown-toggle nav-link" data-toggle="dropdown">
+                        <i class="nc-icon nc-planet"></i>
+                        <span class="notification">5</span>
+                    </a>
+                    <ul class="dropdown-menu">
+                        <a class="dropdown-item" href="#"Notification 1</a>
+                        <a class="dropdown-item" href="#"Notification 2</a>
+                        <a class="dropdown-item" href="#"Notification 3</a>
+                        <a class="dropdown-item" href="#"Notification 4</a>
+                        <a class="dropdown-item" href="#">Another notification</a>
+                    </ul>
+                </li>
+                <li class="nav-item">
+                    <a href="#" class="nav-link">
+                        <i class="nc-icon nc-zoom-split"></i>
+                        <span class="d-lg-block">&nbsp;Search</span>
+                </a>
+            </li>
+        </ul>
+        <ul class="navbar-nav ml-auto">
+                 <li class="nav-item">
+                     <a class="nav-link" href="#pablo">
+                         <span class="no-icon">Account</span>
+                    </a>
+                </li>
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="https://example.com" id="navbarDropdownMenuLink" data-toggle="dropdown">
+                        <span class="no-icon">dropdown</span>
+                    </a>
+                    <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                        <a class="dropdown-item" href="#">Action</a>
+                        <a class="dropdown-item" href="#">Another Action</a>
+                        <a class="dropdown-item" href="#">Something</a>
+                        <a class="dropdown-item" href="#">Something else here</a>
+                        <div class="divider"></div>
+                        <a class="dropdown-item" href="#">Seperated link</a>
+                    </div>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#pablo">
+                        <span class="no-icon">Log out</span>
+                    </a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</nav>
+
+
+                    
+
+
+
+                
